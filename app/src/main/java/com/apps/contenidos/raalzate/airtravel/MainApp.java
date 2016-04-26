f49680b89dfbb0ca34556e63d7d45816912d9d2b
Raul Andres Alzate Gomez <raul.alzate@asesor.une.com.co>
2016-04-26 12:12:00 -0500

    first commit - login

---
+++ app/src/main/java/com/apps/contenidos/raalzate/airtravel/MainApp.java
@@ -0,0 +1,14 @@
+package com.apps.contenidos.raalzate.airtravel;
+
+import android.app.Application;
+
+
+/**
+ * Created by MyMac on 20/02/16.
+ */
+public class MainApp extends Application {
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
